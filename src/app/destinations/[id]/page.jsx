4d08bc47f2d5e9b1be44d768e0bbfe09e51e2430
mdnahidooo <md.nahidofficial.cse@gmail.com>
2026-05-10 06:36:11 +0600

    Destination Details Page

---
+++ src/app/destinations/[id]/page.jsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import React from 'react';
+import { FaRegCalendar } from 'react-icons/fa6';
+import { LuMapPin } from 'react-icons/lu';
+
+const DestinationDetailsPage = async ({ params }) => {
+    const { id } = await params;
+    // console.log(id);
+    const res = await fetch(`http://localhost:5000/destination/${id}`);
+    const destination = await res.json();
+    // console.log(destination);
+
+    const { imageUrl, price, destinationName, duration, country, description } =
+        destination;
+
+    return (
+        <div>
+            <div className="max-w-7xl mx-auto">
+                
+                <Image
+                    className="w-full h-100 object-cover"
+                    alt={destinationName}
+                    src={imageUrl}
+                    height={500}
+                    width={800}
+                />
+
+                <div className="p-2">
+                    <div className="flex items-center gap-1">
+                        <LuMapPin /> <span>{country}</span>
+                    </div>
+                    <div className="flex justify-between">
+                        <div>
+                            <div>
+                                <h2 className="text-xl font-bold">{destinationName}</h2>
+                            </div>
+                            <div className="flex gap-1 items-center">
+                                <FaRegCalendar /> {duration}
+                            </div>
+                        </div>
+
+                        <div>
+                            <h3 className="text-2xl font-bold">$ {price}</h3>
+                        </div>
+                    </div>
+
+                    <h1 className="mt-10 text-2xl font-bold">Overview</h1>
+
+                    <p>{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default DestinationDetailsPage;
